c1dfbd7a3b3d9815697398c4e7f8ea7cca863f0a
Boris Sedov <borissedov@gmail.com>
2023-02-04 20:03:45 +0400

    Inertia physics

--- Assets/Scripts/AddForcePlayerMovement.cs
+++ Assets/Scripts/AddForcePlayerMovement.cs
@@ -4,17 +4,16 @@ using UnityEngine;
 
 public class AddForcePlayerMovement : MonoBehaviour
 {
-    [SerializeField] private float speed;
     [SerializeField] private bool grounded;
     [SerializeField] private float jumpPower;
 
     private Rigidbody2D body;
 
-    public const string Right = "right";
-    public const string Left = "left";
-
-    string BP;
-
+    [SerializeField] private float moveForce;         
+    [SerializeField] private float maxSpeed;     
+ 
+    private Vector2 v;
+    
     void Start()
     {
         body = GetComponent<Rigidbody2D>();
@@ -22,28 +21,44 @@ public class AddForcePlayerMovement : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetKey(KeyCode.RightArrow))
-        {
-            BP = Right;
-        }
-        else if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            BP = Left;
-        }
-        else if (Input.GetKeyDown(KeyCode.UpArrow) && grounded)
-            Jump();
+        // if (Input.GetKey(KeyCode.RightArrow))
+        // {
+        //     BP = Right;
+        // }
+        // else if (Input.GetKey(KeyCode.LeftArrow))
+        // {
+        //     BP = Left;
+        // }
+        // else if (Input.GetKeyDown(KeyCode.UpArrow) && grounded)
+        //     Jump();
+        Debug.Log($"{Input.GetAxis("Horizontal")} - {Input.GetAxis("Vertical")}");
+        v = new Vector2(Input.GetAxis("Horizontal"), 
+            Input.GetAxis("Vertical"));
     }
+    
     private void FixedUpdate()
     {
-        if (BP == Right)
+        // if (BP == Right)
+        // {
+        //     body.AddForce(new Vector2(speed, 0));
+        // }
+        // else if (BP == Left)
+        // {
+        //     body.AddForce(new Vector2(-speed, 0));
+        // }
+        
+        if (grounded && v.y != 0)
         {
-            body.AddForce(new Vector2(speed, 0));
+            body.velocity = new Vector2(body.velocity.x, jumpPower);
+            grounded = false;
         }
-        else if (BP == Left)
+        else
         {
-            body.AddForce(new Vector2(-speed, 0));
+            body.velocity = Vector2.ClampMagnitude(body.velocity, maxSpeed);
+            body.AddForce(v.normalized * moveForce);
         }
     }
+    
     private void Jump()
     {
         body.velocity = new Vector2(body.velocity.x, jumpPower);
