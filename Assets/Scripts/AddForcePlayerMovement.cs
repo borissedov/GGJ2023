f3fb8a3da3c8a97c1226f9f0b79cf10a7918a155
LSTq <mdoronichev@icloud.com>
2023-02-05 15:00:07 +0400

    анимация и управление на всех уровнях

--- Assets/Scripts/AddForcePlayerMovement.cs
+++ Assets/Scripts/AddForcePlayerMovement.cs
@@ -8,6 +8,7 @@ public class AddForcePlayerMovement : MonoBehaviour
     [SerializeField] private float jumpPower;
 
     private Rigidbody2D body;
+    private Animator anim;
 
     [SerializeField] private float moveForce;         
     [SerializeField] private float maxSpeed;     
@@ -17,10 +18,19 @@ public class AddForcePlayerMovement : MonoBehaviour
     void Start()
     {
         body = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     private void Update()
     {
+        float horizontalInput = Input.GetAxis("Horizontal");
+
+        //Flip player when facing left/right.
+        if (horizontalInput > 0.01f)
+            transform.localScale = new Vector3(-1, 1, 1);
+        else if (horizontalInput < -0.01f)
+            transform.localScale = Vector3.one;
+
         // if (Input.GetKey(KeyCode.RightArrow))
         // {
         //     BP = Right;
@@ -34,6 +44,10 @@ public class AddForcePlayerMovement : MonoBehaviour
         // Debug.Log($"{Input.GetAxis("Horizontal")} - {Input.GetAxis("Vertical")}");
         v = new Vector2(Input.GetAxis("Horizontal"), 
             Input.GetAxis("Vertical"));
+
+        //sets animation parameters
+        anim.SetBool("run", horizontalInput != 0);
+        anim.SetBool("grounded", grounded);
     }
     
     private void FixedUpdate()
@@ -68,5 +82,11 @@ public class AddForcePlayerMovement : MonoBehaviour
     {
         if (collision.gameObject.tag == "Ground")
             grounded = true;
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+            grounded = false;
     }
 }
--- Assets/Scripts/Movements/InvertedUncontrolPlayeMovement.cs
+++ Assets/Scripts/Movements/InvertedUncontrolPlayeMovement.cs
@@ -10,23 +10,37 @@ public class InvertedUncontrolPlayeMovement : MonoBehaviour
     [SerializeField] private bool grounded;
 
     private Rigidbody2D body;
+    private Animator anim;
 
     private void Awake()
     {
         body = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     private void Update()
     {
+        float horizontalInput = Input.GetAxis("Horizontal");
+
         if (grounded)
         {
-            body.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * -speed, body.velocity.y);
+            body.velocity = new Vector2(horizontalInput * -speed, body.velocity.y);
         }
+
+        //Flip player when facing left/right.
+        if (horizontalInput > 0.01f)
+            transform.localScale = Vector3.one;
+        else if (horizontalInput < -0.01f)
+            transform.localScale = new Vector3(-1, 1, 1);
 
         if (Input.GetKeyDown(KeyCode.DownArrow) && grounded)
         {
             Jump();
         }
+
+        //sets animation parameters
+        anim.SetBool("run", horizontalInput != 0);
+        anim.SetBool("grounded", grounded);
     }
 
     private void Jump()
